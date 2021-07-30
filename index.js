ae1b1c28da528ee8d59ffd25d3b5c44e072e56f7
Aelbish Shrestha <aelbish@gmail.com>
2021-07-29 22:21:38 -0500

    trying to deploy

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello to InstaMemories API");
+  res.send("Welcome to InstaMemories API");
 });
 
 const PORT = process.env.PORT || 5000;
